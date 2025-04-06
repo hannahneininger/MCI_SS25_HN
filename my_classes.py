3d238728eed6e0446969240fb3ae2e9c62b0a36f
hannahneininger <hannahneininger@gmail.com>
2025-04-06 22:26:33 +0200

    Add files via upload

---
+++ my_classes.py
@@ -0,0 +1,15 @@
+class Subject()
+    def __innit__(self, name)
+    def estimate_max_hr()
+    
+        
+class Supervisor()
+    def __innit__(self, name)
+
+
+class Experiment()
+    def __innit__(self, experiment_name, date, supervisor, subject):
+        self.experiment_name = experiment_name
+        self.date = date
+        self.supervisor = supervisor
+        self.subject = subject
